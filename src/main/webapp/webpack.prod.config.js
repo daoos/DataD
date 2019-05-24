456de2f3000a5df57e44e5b6d8c89fab3cf819b5
yangjijiang <yangjijiang@sina.cn>
2019-05-24 10:39:02 +0800

    打包调整

--- src/main/webapp/webpack.prod.config.js
+++ src/main/webapp/webpack.prod.config.js
@@ -5,17 +5,17 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: 'dist/',
-        path: path.join(__dirname, '../dist/'),
-        filename: '[name].[hash].bundle.js',
-        chunkFilename: '[name].[hash].chunk.js'
+        //publicPath: 'dist/',
+        path: path.join(__dirname, './dist/'),
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].chunk.js'
     },
     module: {
         rules: []
     },
     plugins: [
         new htmlWebpackPlugin({
-            filename: path.join(__dirname, '../index.html'),
+            filename: path.join(__dirname, './dist/index.html'),
             template: path.join(__dirname, './src/template/index.ejs'),
             inject: false,
             // minify:{ //压缩HTML文件
